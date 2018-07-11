053069adb489ffa1be0b77e1d960dcb94a8f4a95
DanielLyleWilliamWarren <daniel.warren.deen@gmail.com>
2018-07-11 15:23:02 +0100

    setup course lesson and student classes

--- src/main/java/models/Lesson.java
+++ src/main/java/models/Lesson.java
@@ -1,4 +1,46 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Lesson {
+    private int id;
+    private String title;
+    private int classNumber;
+
+    public Lesson(){}
+
+    public Lesson(String title, int classNumber) {
+        this.title = title;
+        this.classNumber = classNumber;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Column(name = "title")
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    @Column(name = "class_number")
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
 }
--- src/main/java/models/Student.java
+++ src/main/java/models/Student.java
@@ -1,4 +1,56 @@
 package models;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Student {
+
+    private int id;
+    private String name;
+    private int age;
+    private int enrollmentNumber;
+
+    public Student(){}
+
+    public Student(String name, int age, int enrollmentNumber) {
+        this.name = name;
+        this.age = age;
+        this.enrollmentNumber = enrollmentNumber;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @Column(name = "age")
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Column(name = "enrollment_number")
+    public int getEnrollmentNumber() {
+        return enrollmentNumber;
+    }
+
+    public void setEnrollmentNumber(int enrollmentNumber) {
+        this.enrollmentNumber = enrollmentNumber;
+    }
 }
